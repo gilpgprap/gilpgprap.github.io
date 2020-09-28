a532cb4754c670c44eda14184eb7e4899b588752
gilpgcl <gilpgcl@gmail.com>
2020-09-28 15:18:51 -0500

    Agrega sitio web

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -3,7 +3,7 @@ customElements.define("mi-nav", class extends HTMLElement {
     this.innerHTML = /* html */
       `<h2>Contenido</h2>
       <ul>
-        <li><a href="index.html">Inicio</a></li>
+        <li><a href="index.html">Sitio Web</a></li>
         <li><a href="0_sw.html">Software a Instalar</a></li>
         <li><a href="2_html.html">Introducción a HTML5</a></li>
         <li><a href="4_css3.html">Introducción a CSS3</a></li>
@@ -17,6 +17,7 @@ customElements.define("mi-nav", class extends HTMLElement {
         <li><a href="12_funciones.html">Funciones</a></li>
         <li><a href="13_objetos.html">Objetos</a></li>
         <li><a href="14_excepciones.html">Excepciones</a></li>
+        <li><a href="15_sitio.html">Sitio Web</a></li>
       </ul>`;
   }
 });
---
+++ src/gilpgform/js/ctrlSaludo.js
@@ -0,0 +1,5 @@
+function saluda() {
+  const nombre = inNombre.value;
+  const saludo = `Hola ${nombre}.`;
+  outSaludo.value = saludo;
+}
